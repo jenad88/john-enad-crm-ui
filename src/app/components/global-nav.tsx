33571a1288359a818f82a6767354a6c3e755eb81
John Enad <jenad88@hotmail.com>
2023-07-11 19:47:42 -0400

    refactor(layout): refactor global nav from various segment layouts to the root layout

--- src/app/components/global-nav.tsx
+++ src/app/components/global-nav.tsx
@@ -1,13 +1,25 @@
 'use client'
 
+import Link from 'next/link';
+import { useState } from 'react';
+
 export default function GlobalNav() {
+    const [isOpen, setIsOpen] = useState(false);
+    const close = () => setIsOpen(false);
+
     return (
         <>
             {/* Static sidebar for desktop */}
             <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
                 <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-indigo-600 px-6 pb-4">
                     <div className="flex h-16 shrink-0 items-center">
+                    <Link
+                        href="/"
+                        className="group flex w-full items-center gap-x-2.5"
+                        onClick={close}
+                    >
                         <h2>LOGO</h2>
+                        </Link>
                     </div>
                     <nav className="flex flex-1 flex-col">
                         <ul role="list" className="flex flex-1 flex-col gap-y-7">
